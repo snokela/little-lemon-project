2fa82d5cccd586439e1e5b33590d2b4903f37aac
saananokela <smnokela@gmail.com>
2024-07-14 11:27:31 +0300

    Added the logo img and footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,39 @@
 import React from "react";
+import footerlogo from '../images/footer-logo.png';
 
 const Footer = () => {
   return (
     <footer>
-      <p>Footer content goes here</p>
+      <div className="footer-container">
+        <img src={footerlogo} alt="Litlle lemon logo" className="footer-logo"/>
+        <div className="doormat-navigation">
+          <h6>Doormat Navigation</h6>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#menu">Menu</a></li>
+            <li><a href="#reservations">Reservations</a></li>
+            <li><a href="#orederOnline">Order Online</a></li>
+            <li><a href="#login">Login</a></li>
+          </ul>
+        </div>
+        <div className="contact">
+          <h6>Contact</h6>
+          <ul>
+            <li>Address</li>
+            <li>Phone number</li>
+            <li>email</li>
+          </ul>
+        </div>
+        <div className="social-media">
+          <h6>Social Media Links</h6>
+          <ul>
+            <li>The link to the Facebook icon goes here</li>
+            <li>The link to the Instagram icon goes here</li>
+            <li>The link to the Twitter icon goes here</li>
+          </ul>
+        </div>
+      </div>
     </footer>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,15 @@
 import React from "react";
+import navlogo from '../images/nav-logo.png';
 
 const Nav = () => {
   return (
     <nav>
       <div className="nav-container">
+        <img src={navlogo} alt="Little Lemon Logo" className="nav-logo"/>
         <ul>
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
+          <li><a href="#menu">Menu</a></li>
           <li><a href="#reservations">Reservations</a></li>
           <li><a href="#orederOnline">Order Online</a></li>
           <li><a href="#login">Login</a></li>
